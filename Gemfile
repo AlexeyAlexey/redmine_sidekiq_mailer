0bab962925a2847dfdc2a1ce71339c4ba2133b6b
Alexey Kondratenko <a.kondratenko@smileexpo.ru>
2016-02-24 16:59:05 +0200

    readme

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 #source "https://rubygems.org"
 gem 'sidekiq', :source => 'https://rubygems.org/'
-gem "sidekiq_redmine_mailer"
+gem "sidekiq_redmine_mailer", :git => "https://github.com/AlexeyAlexey/sidekiq_redmine_mailer"
--- init.rb
+++ init.rb
@@ -1,6 +1,17 @@
 #require 'redmine_sidekiq_mailer'
 #require_dependency 'redmine_sidekiq_mailer'
 
+Redmine::Plugin.register :redmine_sidekiq_mailer do
+  name 'Sidekiq Mailer Integration'
+  author 'Alexey Kondratenko'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/AlexeyAlexey/redmine_sidekiq_mailer'
+  author_url 'https://github.com/AlexeyAlexey/redmine_sidekiq_mailer'
+  
+end
+
+
 ActionDispatch::Callbacks.to_prepare do
   Mailer.send(:include, Sidekiq::RedmineMailer)
 
